55781558c954b3ca9d83fb70208c56fe67016327
Caitano <josias.m.caitano@accenture.com>
2024-04-21 15:12:27 -0300

    feature: add bearer token in swagger

--- src/main/java/com/josiasmartins/spring_security_jwt_oauth2/config/SwaggerConfig.java
+++ src/main/java/com/josiasmartins/spring_security_jwt_oauth2/config/SwaggerConfig.java
@@ -1,14 +1,20 @@
 package com.josiasmartins.spring_security_jwt_oauth2.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -17,6 +23,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components() // adiciona os botoes de autenticao
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement() // adiciona a logica de autenticação
+                                .addList("bearer-jwt", Arrays.asList("read", "write"))
+                                .addList("bearer-key", Collections.emptyList())
+                )
                 .info(new Info()
                         .title("Api Tweet simplist")
                         .description("API Rest Tweet")
